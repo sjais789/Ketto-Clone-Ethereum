9c97bac3ade239c0b32c50c0ebce569b0f40b062
Shubham <sjais789@users.noreply.github.com>
2019-04-23 19:11:44 +0200

    Update ketto_contract.sol

--- ketto_contract.sol
+++ ketto_contract.sol
@@ -7,3 +7,11 @@ address public owner;
 
 uint public min_amount;
  address [] public authorized;
+
+
+constructor (uint min) public {    ///see the constructor  syntax
+owner = msg.sender;
+
+min_amount = min;
+
+}
